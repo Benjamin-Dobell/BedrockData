dab9d56c654fbcf563622445c2eba876d0e85228
Benjamin Dobell <git@benjamin.dobell.email>
2023-05-21 19:11:12 +1000

    Export the latest resource structure

--- index.js
+++ index.js
@@ -2,26 +2,41 @@
 const fs = require('fs');
 
 const banner_patterns = require('./resources/banner_patterns.json');
-const block_id_map = require('./resources/block_id_map.json');
+const biomeIdMap = require('./resources/biome_id_map.json');
+const blockIdToItemIdMap = require('./resources/block_id_to_item_id_map.json');
+const blockStateMetaMap = require('./resources/block_state_meta_map.json');
+const commandArgTypes = require('./resources/command_arg_types.json');
 const creativeitems = require('./resources/creativeitems.json');
-const entity_id_map = require('./resources/entity_id_map.json');
-const item_id_map = require('./resources/item_id_map.json');
-const recipes = require('./resources/recipes.json');
+const entityIdMap = require('./resources/entity_id_map.json');
+const itemTags = require('./resources/item_tags.json');
+const levelSoundIdMap = require('./resources/level_sound_id_map.json');
+const particleIdMap = require('./resources/particle_id_map.json');
+const r16ToCurrentItemMap = require('./resources/r16_to_current_item_map.json');
+const requiredItemList = require('./resources/required_item_list.json');
 
-const biome_definitions = fs.readFileSync(__dirname + '/resources/biome_definitions.nbt');
-const entity_identifiers = fs.readFileSync(__dirname + '/resources/entity_identifiers.nbt');
-const canonical_block_states = fs.readFileSync(__dirname + '/resources/canonical_block_states.nbt');
-const r12_to_current_block_map = fs.readFileSync(__dirname + '/resources/r12_to_current_block_map.bin');
+const recipes = require('./recipes');
+
+const biomeDefinitions = fs.readFileSync(__dirname + '/resources/biome_definitions.nbt');
+const canonicalBlockStates = fs.readFileSync(__dirname + '/resources/canonical_block_states.nbt');
+const entityIdentifiers = fs.readFileSync(__dirname + '/resources/entity_identifiers.nbt');
+const r12ToCurrentBlockMap = fs.readFileSync(__dirname + '/resources/r12_to_current_block_map.bin');
 
 module.exports = {
     banner_patterns,
-    biome_definitions,
-    block_id_map,
+    biomeDefinitions,
+    biomeIdMap,
+    blockIdToItemIdMap,
+    blockStateMetaMap,
+    canonicalBlockStates,
+    commandArgTypes,
     creativeitems,
-    entity_id_map,
-    entity_identifiers,
-    item_id_map,
-    r12_to_current_block_map,
+    entityIdentifiers,
+    entityIdMap,
+    itemTags,
+    levelSoundIdMap,
+    particleIdMap,
+    r12ToCurrentBlockMap,
+    r16ToCurrentItemMap,
     recipes,
-    canonical_block_states
+    requiredItemList,
 };
---
+++ recipes.js
@@ -0,0 +1,25 @@
+const potionContainerChange = require('./resources/recipes/potion_container_change.json');
+const potionType = require('./resources/recipes/potion_type.json');
+const shapedChemistry = require('./resources/recipes/shaped_chemistry.json');
+const shapedCrafting = require('./resources/recipes/shaped_crafting.json');
+const shapelessChemistry = require('./resources/recipes/shapeless_chemistry.json');
+const shapelessCrafting = require('./resources/recipes/shapeless_crafting.json');
+const shapelessShulkerBox = require('./resources/recipes/shapeless_shulker_box.json');
+const smelting = require('./resources/recipes/smelting.json');
+const smithing = require('./resources/recipes/smithing.json');
+const smithingTrim = require('./resources/recipes/smithing_trim.json');
+const specialHardcoded = require('./resources/recipes/special_hardcoded.json');
+
+module.exports = [
+    potionContainerChange,
+    potionType,
+    shapedChemistry,
+    shapedCrafting,
+    shapedChemistry,
+    shapedCrafting,
+    shapelessShulkerBox,
+    smelting,
+    smithing,
+    smithingTrim,
+    specialHardcoded,
+];
